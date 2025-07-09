f41e9ce5920ce6779e0bfdbe622e73dab1c0ac39
Mustafa Mestoev <mustafamestoev12345@gmail.com>
2025-07-09 09:12:57 +0300

    пофиксил проблему нахождением модулей для тестов, сохраняю все изменения

--- tests/test_filter.py
+++ tests/test_filter.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from func.filter import filter_rows
 import pytest
 
--- tests/test_reader.py
+++ tests/test_reader.py
@@ -1,7 +1,11 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from func.reader import read_csv
 
 def test_read_csv():
-    rows = read_csv("tests/fixtures/sample.csv")
+    rows = read_csv("tests/sample.csv")
     assert isinstance(rows, list)
     assert isinstance(rows[0], dict)
     assert rows[0]["name"] == "iphone 15 pro"
